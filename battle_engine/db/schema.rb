d9e285047056fed227e367cea1cbc33aadc7591b
Manchagd <mancha0703@gmail.com>
2026-05-28 13:48:49 -0500

    cleaning trash

--- battle_engine/db/schema.rb
+++ battle_engine/db/schema.rb
@@ -10,14 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_28_175905) do
+ActiveRecord::Schema[7.2].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "trainers", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "level", default: 1, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
