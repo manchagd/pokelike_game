a3bd2e0374e32cd6cbaabd460f0ee57d20e770eb
Manchagd <mancha0703@gmail.com>
2026-05-28 17:12:33 -0500

    fixes

--- battle_engine/config/environment.rb
+++ battle_engine/config/environment.rb
@@ -10,12 +10,25 @@ require_relative "logger"
 require_relative "database"
 require_relative "rabbitmq"
 
-# Setup Zeitwerk autoloader for app/ directory
+# Setup Zeitwerk autoloader for app/ subdirectories
 loader = Zeitwerk::Loader.new
 app_dir = File.expand_path("../app", __dir__)
-loader.push_dir(app_dir)
-loader.ignore("#{app_dir}/models")
-loader.push_dir("#{app_dir}/models")
+global_dirs = %w[models]
+
+Dir.children(app_dir).each do |child|
+  child_path = File.join(app_dir, child)
+  next unless File.directory?(child_path)
+
+  if global_dirs.include?(child)
+    loader.push_dir(child_path)
+  else
+    namespace_name = child.camelize
+    Object.const_set(namespace_name, Module.new) unless Object.const_defined?(namespace_name)
+    namespace = Object.const_get(namespace_name)
+    loader.push_dir(child_path, namespace: namespace)
+  end
+end
+
 loader.setup
 
 BattleEngine.logger.info("[Environment] Loaded (#{AppConfig.app_env})")
