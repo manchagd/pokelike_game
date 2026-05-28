5f9e98a7f96fe070ea637ae657335f3fb4d4c51f
Manchagd <mancha0703@gmail.com>
2026-05-28 13:37:54 -0500

    feat: auto-dump db/schema.rb after running migrations and rollbacks
    - config/database.rb: added dump_schema! helper method that uses ActiveRecord::SchemaDumper on connection_pool.
    - scripts/db_migrate: invokes dump_schema! upon completion.
    - scripts/db_rollback: invokes dump_schema! upon completion.
    - db/schema.rb: updated automatically with the schema after migrating.

--- battle_engine/config/database.rb
+++ battle_engine/config/database.rb
@@ -18,5 +18,15 @@ module BattleEngine
         url:     AppConfig.database_url
       }
     end
+
+    def dump_schema!
+      require "active_record/schema_dumper"
+      schema_file = File.expand_path("../db/schema.rb", __dir__)
+
+      File.open(schema_file, "w:utf-8") do |file|
+        ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection_pool, file)
+      end
+      BattleEngine.logger.info("[DB] Schema successfully dumped to db/schema.rb")
+    end
   end
 end
