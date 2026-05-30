8359d6b56085a3d1d7d895538ff88a8fe2fb7939
tutto2 <tuto.2@hotmail.com>
2026-05-30 10:08:31 -0500

    migrations run and schema update

--- battle_engine/db/schema.rb
+++ battle_engine/db/schema.rb
@@ -10,9 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_28_232503) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_30_135441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "battle_players", force: :cascade do |t|
+    t.bigint "battle_id", null: false
+    t.bigint "player_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["battle_id"], name: "index_battle_players_on_battle_id"
+    t.index ["player_id"], name: "index_battle_players_on_player_id"
+  end
+
+  create_table "battles", force: :cascade do |t|
+    t.string "battle_type", default: "single", null: false
+    t.bigint "field_id", null: false
+    t.bigint "winner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["field_id"], name: "index_battles_on_field_id"
+    t.index ["winner_id"], name: "index_battles_on_winner_id"
+  end
+
+  create_table "fields", force: :cascade do |t|
+    t.jsonb "positions", default: {}, null: false
+    t.bigint "weather_id"
+    t.bigint "hazard_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hazard_id"], name: "index_fields_on_hazard_id"
+    t.index ["weather_id"], name: "index_fields_on_weather_id"
+  end
+
+  create_table "hazards", force: :cascade do |t|
+    t.string "name", null: false
+    t.jsonb "effect", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_hazards_on_name", unique: true
+  end
 
   create_table "moves", force: :cascade do |t|
     t.string "name", null: false
@@ -26,6 +63,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_28_232503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_moves_on_name", unique: true
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_players_on_name", unique: true
   end
 
   create_table "pokemon_template_moves", force: :cascade do |t|
@@ -46,6 +90,63 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_28_232503) do
     t.index ["name"], name: "index_pokemon_templates_on_name", unique: true
   end
 
+  create_table "pokemons", force: :cascade do |t|
+    t.bigint "pokemon_template_id", null: false
+    t.bigint "player_id", null: false
+    t.string "nickname"
+    t.string "gender"
+    t.string "nature", null: false
+    t.decimal "weight", precision: 8, scale: 2, null: false
+    t.jsonb "ivs", default: {}, null: false
+    t.jsonb "evs", default: {}, null: false
+    t.string "attacks", default: [], null: false, array: true
+    t.integer "lvl", null: false
+    t.jsonb "metadata", default: {}, null: false
+    t.string "teratype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_pokemons_on_player_id"
+    t.index ["pokemon_template_id"], name: "index_pokemons_on_pokemon_template_id"
+  end
+
+  create_table "team_pokemons", force: :cascade do |t|
+    t.bigint "team_id", null: false
+    t.bigint "pokemon_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pokemon_id"], name: "index_team_pokemons_on_pokemon_id"
+    t.index ["team_id"], name: "index_team_pokemons_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "player_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_teams_on_player_id"
+  end
+
+  create_table "weathers", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "duration", default: 5, null: false
+    t.integer "harm"
+    t.jsonb "type_modifiers", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_weathers_on_name", unique: true
+  end
+
+  add_foreign_key "battle_players", "battles"
+  add_foreign_key "battle_players", "players"
+  add_foreign_key "battles", "fields"
+  add_foreign_key "battles", "players", column: "winner_id"
+  add_foreign_key "fields", "hazards"
+  add_foreign_key "fields", "weathers"
   add_foreign_key "pokemon_template_moves", "moves"
   add_foreign_key "pokemon_template_moves", "pokemon_templates"
+  add_foreign_key "pokemons", "players"
+  add_foreign_key "pokemons", "pokemon_templates"
+  add_foreign_key "team_pokemons", "pokemons"
+  add_foreign_key "team_pokemons", "teams"
+  add_foreign_key "teams", "players"
 end
