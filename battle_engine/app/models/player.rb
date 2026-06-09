b0bff43b4d88325fef6b5973393f20ce81dee257
Manchagd <mancha0703@gmail.com>
2026-06-09 16:40:27 -0500

    logic

--- battle_engine/app/models/player.rb
+++ battle_engine/app/models/player.rb
@@ -2,9 +2,9 @@
 
 class Player < ApplicationRecord
   has_many :teams
+  has_and_belongs_to_many :battles, join_table: "battle_players"
 
   validates :name, presence: true, uniqueness: true
-
 
   def info_event_payload
     {
@@ -13,12 +13,29 @@ class Player < ApplicationRecord
         name:,
         teams: [],
         battle_history: {
-          victories: 0,
-          defeats: 0,
-          history: []
-      }
+          victories: battles.select { |b| b.winner?(self) }.count,
+          defeats: battles.select { |b| !b.winner?(self) }.count,
+          history: battles.last(10).map { |b| b.winner?(self) ? "V" : "D" }
+        }
       },
-      battles: []
+      battles: battle_info_event_payload
     }
+  end
+
+  def battle_info_event_payload
+    battles.running.map do |battle|
+      { 
+        id: battle.id, 
+        opponent: battle_player_names(battle.players)
+      } 
+    end
+  end
+
+  private
+
+  def battle_player_names(players)
+    # TODO: Ajustar a equipos
+    # {name: "player_x", team: "A"} -> nombre del jugador y su equipo
+    players.filter { |p| p.id != id }.map(&:name).join(", ")
   end
 end
