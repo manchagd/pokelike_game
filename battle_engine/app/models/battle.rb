51158ac88e8a53ded66ab3e10905a83895c361c3
Carlos Augusto Herrera Orozco <carlosh36@gmail.com>
2026-06-12 17:07:40 -0500

    add status column to battles

--- battle_engine/app/models/battle.rb
+++ battle_engine/app/models/battle.rb
@@ -8,9 +8,9 @@ class Battle < ApplicationRecord
   
   before_create :set_external_id
 
-  # Logica para identificar o status da battle
+  enum status: { not_started: "not_started", in_progress: "in_progress", finished: "finished" }
+  
   # Logica de equipos, y jugadores
-  scope :running, -> { all }
 
   def winner?(player)
     winner_id == player.id
@@ -20,8 +20,8 @@ class Battle < ApplicationRecord
 
   def set_external_id
     self.external_id = loop do
-      first_chain = SecureRandom.alphanumeric(3).upcase
-      second_chain = SecureRandom.alphanumeric(3).upcase
+      first_chain = SecureRandom.alphanumeric(3)
+      second_chain = SecureRandom.alphanumeric(3)
       chain = "#{first_chain}-#{second_chain}"
       
       break chain unless Battle.exists?(external_id: chain)
--- battle_engine/db/schema.rb
+++ battle_engine/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_12_210039) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_12_213314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_12_210039) do
     t.datetime "updated_at", null: false
     t.integer "turn", default: 0, null: false
     t.string "external_id"
+    t.string "status", default: "not_started", null: false
     t.index ["external_id"], name: "index_battles_on_external_id", unique: true
     t.index ["field_id"], name: "index_battles_on_field_id"
     t.index ["winner_id"], name: "index_battles_on_winner_id"
