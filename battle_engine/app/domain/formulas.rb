927facc5bd495dd0f1b8e1000e718f307abb1674
Manchagd <mancha0703@gmail.com>
2026-06-19 19:48:05 -0500

    wip wip
    wip

--- battle_engine/app/domain/formulas.rb
+++ battle_engine/app/domain/formulas.rb
@@ -20,4 +20,40 @@ module Formulas
     raw_stat = ((((2 * base_stat) + iv_stat + (ev_stat / 4)) * level) / 100).floor + 5
     (raw_stat * nature_modifier).floor
   end
+
+  def self.damage_formula(
+    attack:,
+    defense:,
+    lvl:,
+    power:,
+    targets: 1,
+    pb: 1,
+    weather: 1,
+    glaive_rush: 1,
+    critical: 1,
+    stab: 1,
+    type: 1,
+    burn: 1,
+    other: 1,
+    z_move: 1,
+    terashield: 1
+  )
+    attack = attack.to_i
+    defense = defense.to_i
+    lvl = lvl.to_i
+    power = power.to_i
+    targets = targets.to_i
+    pb = pb.to_i
+    weather = weather.to_i
+    glaive_rush = glaive_rush.to_i
+    critical = critical.to_i
+    stab = stab.to_i
+    type = type.to_i
+    burn = burn.to_i
+    other = other.to_i
+    z_move = z_move.to_i
+    terashield = terashield.to_i
+
+    (((((2 * lvl) / 5).floor + 2) * power * (attack / defense) / 50) + 2) * targets * pb * weather * glaive_rush * critical * stab * type * burn * other * z_move * terashield
+  end
 end
